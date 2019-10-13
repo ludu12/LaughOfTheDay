358cf42273811920f20f252e8610cf7b298e897e
ahallier <arhallier@gmail.com>
2019-10-13 18:57:12 -0500

    adding fetch for pic list

--- src/components/App.js
+++ src/components/App.js
@@ -8,17 +8,29 @@ const options = [
 
 export const App = () => {
     const [picture, setPicture] = React.useState(options[0].value);
+    const [pictureList, setPictureList] = React.useState([]);
 
     const handleSelect = (e) => {
         setPicture(e.target.value);
     };
+
+    React.useEffect(()=> {
+        fetch('https://ld5whwmgo8.execute-api.ca-central-1.amazonaws.com/prod/getPhoto')
+        .then(res => res.json())
+        .then((data) => {
+          console.log('Got a response')
+          const picturesArray = data.Items.map(function(photo){return photo.Name})
+          setPictureList(picturesArray)
+        })
+        .catch(console.log)
+    })
 
     return (
         <div className='App'>
             <div className='App-header'>
                 <h1>Puzzle It</h1>
                 <select defaultValue={picture} onChange={handleSelect}>
-                    {options.map(o => (<option key={o.value} value={o.value}>{o.label}</option>))}
+                    {pictureList.map(o => (<option key={o} value={o}>{o}</option>))}
                 </select>
             </div>
             <Puzzle picture={picture}/>
